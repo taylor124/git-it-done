247fde7dbeaa8e5425bfda414859d190eb7c6716
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-01-21 12:45:01 -0500

    feature: linking the two html

--- module-6-starter/module-6-starter/assets/js/single.js
+++ module-6-starter/module-6-starter/assets/js/single.js
@@ -1,20 +1,51 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  }
+  else {
+    document.location.replace("./index.html");
+  }
+}
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-  fetch(apiUrl).then(function (response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function (data) {
-        // pass response data to dom function
-        displayIssues(data);
-      });
-    }
-    else {
-      alert("There was a problem with your request!");
-    }
-  });
+  // make a get request to url
+fetch(apiUrl).then(function(response) {
+  // request was successful
+  if (response.ok) {
+    response.json().then(function(data) {
+      displayIssues(data);
+
+      // check if api has paginated issues
+      if (response.headers.get("Link")) {
+        displayWarning(repo);
+      }
+    });
+  } else {
+    // if not successful, redirect to homepage
+    document.location.replace("./index.html");
+  }
+});
 };
 
 var displayIssues = function (issues) {
@@ -53,4 +84,4 @@ var displayIssues = function (issues) {
 
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
